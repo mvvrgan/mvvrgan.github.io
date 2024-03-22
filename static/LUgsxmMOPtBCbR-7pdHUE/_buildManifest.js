bc412915b2acda643a808f7642f82880091a4738
Morgan Dale <morgzgtalive@gmail.com>
2024-03-22 21:03:09 +0000

    Updates

---
+++ static/LUgsxmMOPtBCbR-7pdHUE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/386-4cd2a5281d2cd6a7.js","static/chunks/pages/index-2b714584e23189c9.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
